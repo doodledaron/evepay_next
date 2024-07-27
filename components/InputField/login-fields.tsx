63401bed8238b50c3739825371a10042526773cf
Jing <ongjingrou090@gmail.com>
2024-07-27 21:47:29 +0800

    test run iconify

--- components/InputField/login-fields.tsx
+++ components/InputField/login-fields.tsx
@@ -1,15 +1,15 @@
-// import { Icon } from "@iconify/react";
+import { Icon } from "@iconify/react";
 import React, { useState } from "react";
 
 const LoginFields = () => {
   return (
     <div>
       <div className="mt-12 flex items-center border border-gray rounded-lg p-4">
-        {/* <Icon
+        <Icon
           icon="fa6-solid:wallet"
           style={{ color: "#72ACA9", fontSize: "24px" }}
           className="mx-3 mr-4"
-        /> */}
+        />
         <input
           type="text"
           placeholder="Wallet ID"
@@ -17,11 +17,11 @@ const LoginFields = () => {
         />
       </div>
       <div className="mt-12 flex items-center border border-gray rounded-lg p-4">
-        {/* <Icon
+        <Icon
           icon="mdi:password"
           style={{ color: "#72ACA9", fontSize: "24px" }}
           className="mx-3  mr-4"
-        /> */}
+        />
         <input
           type="password"
           placeholder="Password"
