830d8728ca168e0ee7e34d676b9b858d3687eb67
Jing <ongjingrou090@gmail.com>
2024-07-27 21:15:06 +0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import NextButton from "../components/Button/next-button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col mt-16 items-center">
+      <Image src="/main-page.jpg" alt="Main Page" width={421} height={396} />
+
+      <p className="mt-5 text-center font-semibold text-gray">
+        Get ready for seamless and secure charging?
+      </p>
+      <p className="text-center font-semibold text-gray">
+        Login to your <span className="text-light-cyan">Maschain wallet</span>.
+      </p>
+
+      <NextButton urlLink="/login" buttonText="Get Started" />
+    </div>
+  );
+}
---
+++ components/Button/next-button.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+interface ButtonProps {
+  urlLink: string;
+  buttonText: string;
+}
+
+const NextButton: React.FC<ButtonProps> = ({ urlLink, buttonText }) => {
+  return (
+    <Link
+      href={urlLink}
+      className="fixed bottom-24 w-4/5 self-center"
+      // className="w-full"
+      // className="absolute bottom-14 left-1/2 transform -translate-x-1/2 w-full max-w-sm"
+    >
+      <p className="mt-16 bg-light-cyan text-white text-center font-bold py-4 px-4 rounded-full hover:opacity-90">
+        {buttonText}
+      </p>
+    </Link>
+
+  );
+};
+
+export default NextButton;
