b3f0b2290114b900f5c4ee08df59702b1eca5050
Jing <ongjingrou090@gmail.com>
2024-07-27 21:34:06 +0800

    test iconify

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        'light-red': '#F38181',
+        'light-cyan': '#72ACA9',
+        'lg-light-cyan': '#89B0AE',
+        'dark-green': '#008000',
+        'dark-green-transparent': 'rgba(0, 128, 0, 0.42)', 
+        'gray': '#9A9A9A',
+        'light-gray': '#FAF9F9',
+        'xs-light-gray': '#959595',
+        'light-blacK': '#555B6E'
+      }
     },
   },
   plugins: [],
